113db71258c01b37ef0797d07c59abd9015e6ead
kennyfrankenstein <kennethanthony42@gmail.com>
2019-03-31 16:43:48 +0530

    .

--- includes/RegisterPetDetails.php
+++ includes/RegisterPetDetails.php
@@ -1,28 +1,28 @@
 <?php
-$con = mysqli_connect("localhost","root","");
-if(!con)
+$con = mysqli_connect('localhost','root','');
+if(!$con)
 {
-	echo'not connected to the server'
+	echo'not connected to the server';
 }
-if (!mysqli_select_db($con,'pets'));
+if (!mysqli_select_db($con,'pets'))
 	{
 
 		echo'database not selected';
 		}
 
-	$UserName = $_POST["username"];
-	$password = $_POST["pwd"];
-	$PetName = $_POST["Petname"];
-	$PetColor = $_POST["Petcolor"];
-	$PetBreed = $_POST["PetBreed"];
-	$DOB = $_POST["dob"];
-	$email = $_POST["email"];
-	$address= $_POST["add"];
+	$UserName = $_POST['username'];
+	$password = $_POST['pwd'];
+	$PetName = $_POST['Petname'];
+	$PetColor = $_POST['Petcolor'];
+	$PetBreed = $_POST['PetBreed'];
+	$DOB = $_POST['dob'];
+	$email = $_POST['email'];
+	$address= $_POST['add'];
 
 
 
 $sql = "INSERT INTO `registerpetdetails`(`Username`, `password`, `PetName`, `PetColor`, `PetBreed`, `DOB`, `Email`, `Address`) VALUES ('$UserName'),('$pwd'),`$PetName`,`$PetColor`,`$PetBreed`,'$DOB','$email','$Address')";
-if (!mysqli_query($con,$sql));
+if (!mysqli_query($con,$sql))
 {
 	echo 'Not Inserted';
 }
