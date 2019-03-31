fd3ce3cd57d6f4f43f4e5a4f0edad7fcdd228420
kennyfrankenstein <kennethanthony42@gmail.com>
2019-03-31 15:58:51 +0530

    .

--- includes/RegisterPetDetails.php
+++ includes/RegisterPetDetails.php
@@ -1,19 +1,50 @@
 <?php
-
-$UserName = $_POST["usernameName"];
-//$UserName = $_POST["username"];
+$UserName = $_POST["username"];
 $password = $_POST["pwd"];
 $PetName = $_POST["Petname"];
-$Petcolor = $_POST["Petcolor"]
-$PetBreed = $_POST["PetBreed"]
-$dob = $_POST["dob"];
+$PetColor = $_POST["Petcolor"];
+$PetBreed = $_POST["PetBreed"];
+$DOB = $_POST["dob"];
 $email = $_POST["email"];
 $address= $_POST["add"];
 
 
 
-$con = mysqli_connect("localhost","root","","UpdatePetDetails");
+<?php
+$con = mysqli_connect("localhost","root","");
+$db = mysqli_select_db('pets');
 	if(!$con)
 	{
-		die("Cannot connect to DB server");
+			echo 'successfully connected to the database';
+		}else {
+			// code...
+	die("Cannot connect to DB server");
 	}
+	?>
+
+
+
+
+<?php
+$con = mysqli_connect("localhost","root","");
+$db = mysqli_select_db('pets');
+	if(!$con)
+	{
+			echo 'successfully connected to the database';
+		}else {
+			// code...
+	die("Cannot connect to DB server");
+	}
+	?>
+
+$sql = "INSERT INTO `registerpetdetails`(`Username`, `Password`, `PetName`, `PetColor`, `PetBreed`, `DOB`, `Email`, `Address`) VALUES ('$UserName'),('$password'),`$PetName`,`$PetColor`,`$PetBreed`,'$DOB','$Email','$Address')";
+if (!mysqli_query($con,$sql));
+{
+	echo 'Not Inserted';
+}
+else
+{
+	echo 'inserted';
+}
+header("refresh:2; url=RegisterPetDetails.html");
+?>
