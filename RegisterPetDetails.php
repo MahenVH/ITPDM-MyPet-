7da66c65e7e3b0970ce185a7cfb9df4e2fe28292
kennyfrankenstein <kennethanthony42@gmail.com>
2019-03-31 19:46:28 +0530

    .

--- RegisterPetDetails.php
+++ RegisterPetDetails.php
@@ -4,24 +4,24 @@ if(!$con)
 {
 	echo'not connected to the server';
 }
-if(!mysqli_select_db($con,'pets'))
+if (!mysqli_select_db($con,'pets'))
 	{
 
 		echo'database not selected';
 		}
 
-	$UserName = $_POST['UserName'];
-	$password = $_POST['password'];
-	$PetName = $_POST['Pet Name'];
-	$PetColor = $_POST['Pet Color'];
-	$PetBreed = $_POST['Pet Breed'];
-	$DOB = $_POST['DOB'];
-	$email = $_POST['email'];
-	$address= $_POST['address'];
+		$Name = $_POST['UserName'];
+		$password = $_POST['password'];
+		$PetName = $_POST['petname'];
+		$PetColor = $_POST['PetColor'];
+		$PetBreed = $_POST['PetBreed'];
+		$DOB = $_POST['DOB'];
+		$email = $_POST['email'];
+		$address= $_POST['address'];
 
 
 
-$sql = "INSERT INTO `registerpetdetails`(`Username`, `Password`, `Pet Name`, `Pet Color`, `Pet Breed`, `DOB`, `email`, `address`) VALUES ('$UserName'),('$password'),`$PetName`,`$PetColor`,`$PetBreed`,'$DOB','$email','$address')";
+$sql = "INSERT INTO registerpetdetails (`Name`, `Password`, `PetName`, `PetColor`, `PetBreed`, `DOB`, `Email`, `address`) VALUES ('$UserName'),('$password'),`$PetName`,`$PetColor`,`$PetBreed`,'$DOB','$email','$address')";
 if (!mysqli_query($con,$sql))
 {
 	echo 'Not Inserted';
