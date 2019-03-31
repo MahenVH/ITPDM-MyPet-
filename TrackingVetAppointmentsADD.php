e73e0825095d6caea833832e34cdbce02b96d8e1
nimawapt <purnioffic@gmail.com>
2019-03-31 19:44:10 +0530

    pulling purposes

--- TrackingVetAppointmentsADD.php
+++ TrackingVetAppointmentsADD.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <title>TrackingVetAppointmentsADD</title>
-
+<<<<<<< HEAD
     <link rel="stylesheet" type="text/css" href=".\css\VeteriarianAppointmentsADD.css"/>
     <script type="text/javascript">
 
@@ -83,9 +83,9 @@ function validate()
 
 </script>
 
-
+=======
     <link rel="stylesheet" type="text/css" href=".\css\VeteriarianAppointmentsADD.css"/>
-
+>>>>>>> 4efac57bc12102675e4bc9830749e2a3c1d4678f
   </head>
   <body>
     <header>
@@ -95,17 +95,17 @@ function validate()
         <nav>
           <ul>
             <li ><a href="HomePage.html">Home</a></li>
-            <li ><a href="Gallery.php">Gallery</a></li>
-            <li><a href="RegisterPetDetails.html">RegisterPet</a></li>
+            <li class="current"><a href="Gallery.html">Gallery</a></li>
+            <li><a href="RegisterPetDetails.html">Register Your Pet</a></li>
             <li><a href="login.html">Login</a></li>
-            <li ><a href="TrackingVetAppointments_VIEW_UPDATE_DELETE.html">Modify</a></li>
-            <li class="current"><a href="TrackingVetAppointmentsADD.html">Add </a></li>
+            <li ><a href="TrackingVetAppointments_VIEW_UPDATE_DELETE.html">View Update Delete</a></li>
+            <li class=current><a href="TrackingVetAppointmentsADD.html">Add </a></li>
           </ul>
         </nav>
       </div>
 
     </header>
-    <form >
+    <form action=".\includes\vetappadd.php" method="post">
       <div class="center">
       <label for="entryno">Entry No.</label>
       <input type="number" id="entryno" name="entryno" value="">
--- includes/vetappadd.php
+++ includes/vetappadd.php
@@ -2,11 +2,12 @@
 $entry=$_POST("entryno");
 $petname=$_POST("petname");
 $vetname=$_POST("vetname");
+$vetclinicname=$_POST("vetclinic");
 $appday=$_POST("appday");
 $apptime=$_POST("apptime");
 $email=$_POST("email");
 
-$con=mysqli_connect("loacalhost","root","","MyPet");
+$con=mysqli_connect("loacalhost","","","MyPet");
 
 if(!$con)
 {
@@ -22,5 +23,5 @@ $sql="INSERT INTO 'MyPet','VetAppointment'(
   'email')
   VALUES ('".$entry."','".$petname."','".$vetname."','".$appday."','".$apptime."','".$email."');";
 
-
+mysqli_close($con);
  ?>
