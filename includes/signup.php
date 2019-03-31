e0f7e2ada2ef36e56470b47d9ae4fbb80ff3b302
nethsaradilshan <nethsaradilshan97@gmail.com>
2019-03-31 13:43:40 +0530

    edit nav

--- includes/signup.php
+++ includes/signup.php
@@ -6,7 +6,7 @@ $first = mysqli_real_escape_string($conn, $_POST['first']);
 $first = mysqli_real_escape_string($conn, $_POST['last']);
 $first = mysqli_real_escape_string($conn, $_POST['email']);
 $first = mysqli_real_escape_string($conn, $_POST['pwd']);
-$first = mysqli_real_escape_string($conn, $_POST['cpwd']);
+
 
 //Error handlers
 //check for empty fields
@@ -17,7 +17,7 @@ if (empty($first) || empty($last)|| empty($email) ||empty($pwd) || empty($cpwd))
 }else {
 
     //check if input charactors are valid
-    if (!preg_match("/^[a-zA-Z]*$", $first) || !preg_match("/^[a-zA-Z]*$", $last)) {
+    if (!preg_match("/^[a-zA-Z]*$/", $first) || !preg_match("/^[a-zA-Z]*$", $last)) {
       header("Location: ../signup.php?signup=invalid");
       exit();
     }
@@ -29,7 +29,25 @@ if (empty($first) || empty($last)|| empty($email) ||empty($pwd) || empty($cpwd))
       }
       else {
         $sql = "SELECT * FROM user where user_uid='$uid'";
-        $result = mysql
+        $result = mysql_quey_num_row($result);
+        $resultCheck = mysql_num_rows($result);
+
+        if ($resultCheck>0) {
+          header("Location: ../signup.php?signup=usertaken");
+          exit();
+
+          else {
+            //hashing the password
+            $hashpwd = password_hash($pwd, PASSWORD_DEFAULT);
+            //Insert the user in tho the DB
+            $sql = "Insert into user(user_first, user_last, user_email, user_uid, user_pwd)
+             values('$first, '$last', ;'$email', '$uid', $hashpwd);";
+             $result= mysql_quey($conn,$sql);
+             header("Location: ../signup.php?signup=success");
+             exit();
+
+          }
+        }
       }
     }
 }
