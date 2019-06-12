5bcf95e40f6b95ea9867e87e7f58f7db35273855
nimawapt <purnioffic@gmail.com>
2019-06-12 19:04:24 +0530

    Vet appointment view works

--- TrackingVetAppointments_VIEW_UPDATE_DELETE.php
+++ TrackingVetAppointments_VIEW_UPDATE_DELETE.php
@@ -50,77 +50,37 @@
               </th>
             </tr>
             <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
-            <tr>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-              <td></td>
-            </tr>
+
+            <?php
+
+            $servername = "localhost";
+            $username = "root";
+            $password = "";
+            $dbname = "mypet";
+
+            $conn = mysqli_connect($servername,$username,$password,$dbname);
+
+            if ($conn->connect_error) {
+              die("Connection failed".$conn-> connect_error);
+            }
+
+
+            $sql="SELECT entry,petname,vetname,vetclinicname,appday,apptime,email FROM vetappointment";
+            $result =$conn-> query($sql);
+            if ($result-> num_rows > 0) {
+              while ($row = $result-> fetch_assoc()) {
+                echo "<tr><td>".$row["entry"]."</td><td>".$row["petname"]."</td><td>".$row["vetname"]."</td><td>".$row["vetclinicname"]."</td><td>".$row["appday"]."</td><td>".$row["apptime"]."</td><td>".$row["email"]."</td></tr>";
+
+              }
+              echo "</table>";
+            }
+            else {
+              echo "0 result";
+            }
+            $conn->close();
+            ?>
+
+
           </table>
 
           <div>
